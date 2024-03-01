071cd725169029d219493a077b7ff09ce21eee20
sandipkatel <babusandipkatel@gmail.com>
2024-03-02 00:11:23 +0545

    nothing is well

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    popup = Popup(title="Select a Date", size_hint=(None, None), size=(400, 400))
-        date_picker = DatePicker()
-        date_picker.bind(on_date=self.update_date_input)
-        popup.content = date_picker
-        popup.open()
