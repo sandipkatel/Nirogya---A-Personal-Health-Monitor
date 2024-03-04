96a9cab6b776845267dbf32969d6b6aad53921e1
saphalr <saphalrimal@gmail.com>
2024-03-04 14:12:53 +0545

    scheduler

--- visual.py
+++ visual.py
@@ -1,21 +1,22 @@
 from kivymd.app import MDApp
-from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.pickers import MDDatePicker, MDTimePicker
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
-from kivy.properties import ObjectProperty
+# from kivy.properties import ObjectProperty
 from kivy.metrics import dp
 from scheduler import AppointmentScheduler
 import predictor as pr
-
-Window.size = (360, 640)
+from datetime import datetime, date
 
 
 class BackgroundLayout(FloatLayout):
@@ -25,8 +26,10 @@ class BackgroundLayout(FloatLayout):
 class HomeWindow(Screen):
     pass
 
+
 class RoundedButton(Button):
     pass
+
 
 class PredictorWindow(Screen):
     def predict_disease(self):
@@ -45,7 +48,8 @@ class PredictorWindow(Screen):
             self.ids.symptom_container.clear_widgets()
 
             for symptom in symptoms_list:
-                symptom_layout = BoxLayout(orientation='horizontal', size_hint_y=None, height=dp(20))
+                symptom_layout = BoxLayout(
+                    orientation='horizontal', size_hint_y=None, height=dp(20))
 
                 label = Label(text=symptom, size_hint_x=0.8)
                 checkbox = CheckBox()
@@ -59,8 +63,10 @@ class PredictorWindow(Screen):
         else:
             print("Symptoms list is empty or None")
 
+
 class SymptomsWindow(Screen):
     disease_label = None
+
     def find_symptoms(self, disease):
         self.disease_label = disease
         result = pr.dis_symptoms(disease)
@@ -71,14 +77,17 @@ class SymptomsWindow(Screen):
             self.ids.symptoms.text = "Sorry, unable to find any disease in dataset."
 
     def add_info_button(self):
-        content = BoxLayout(orientation='vertical', pos_hint={"top": 0.5, "right": 0.1}, size_hint=(None, None))        
-        content.add_widget(Label(text="Do you want to see prevention and cure?"))
+        content = BoxLayout(orientation='vertical', pos_hint={
+                            "top": 0.5, "right": 0.1}, size_hint=(None, None))
+        content.add_widget(
+            Label(text="Do you want to see prevention and cure?"))
         yes_button = Button(text='Yes')
         no_button = Button(text='No')
         content.add_widget(yes_button)
         content.add_widget(no_button)
-        
-        yes_button.bind(on_release=lambda _: self.display_additional_info(content))
+
+        yes_button.bind(
+            on_release=lambda _: self.display_additional_info(content))
         no_button.bind(on_release=lambda _: self.clear_instance(content))
         self.ids.symptoms.remove_widget(content)
         self.ids.symptoms.add_widget(content)
@@ -92,89 +101,96 @@ class SymptomsWindow(Screen):
     def clear_instance(self, content):
         self.ids.symptoms.remove_widget(content)
         content = BoxLayout(orientation='vertical')
-        content.add_widget(Label(text="Do you want to see prevention and cure?"))
+        content.add_widget(
+            Label(text="Do you want to see prevention and cure?"))
 
 
 class Content(BoxLayout):
     pass
 
-class SchedulerWindow(Screen):
-    dialog = None
 
-    def build(self):
-        self.scheduler = AppointmentScheduler()
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Blue"
-        return Builder.load_string(KV)
+class SchedulerWindow(Screen):
+    scheduler = AppointmentScheduler()
 
     def save_appointment(self):
-        date_str = self.dialog.content_cls.ids.date_input.text
-        time_str = self.dialog.content_cls.ids.time_input.text
-        name = self.dialog.content_cls.ids.name_input.text
+        date_str = self.ids.date_input.text
+        time_str = self.ids.time_input.text
+        name = self.ids.name_input.text
         try:
-            appointment_time = datetime.strptime(date_str + ' ' + time_str, '%Y-%m-%d %H:%M')
+            appointment_time = datetime.strptime(
+                date_str + ' ' + time_str, '%Y-%m-%d %H:%M')
             self.scheduler.schedule_appointment(appointment_time, name)
             self.update_appointments_label()
-            self.dialog.dismiss()
         except ValueError:
             self.show_error_popup('Invalid Date/Time Format')
 
     def update_appointments_label(self):
         appointments = self.scheduler.appointments.queue
-        appointment_texts = [f'{app[1][0]} - {app[1][1]}' for app in appointments]
+        appointment_texts = [
+            f'{app[1][0]} - {app[1][1]}' for app in appointments]
         if appointment_texts:
             appointments_text = '\n'.join(appointment_texts)
         else:
             appointments_text = "No appointments scheduled."
-        self.root.ids.appointments_label.text = f'Appointments:\n{appointments_text}'
+        self.ids.appointments_label.text = f'Appointments:\n{appointments_text}'
 
-    def show_add_appointment_popup(self):
-        if not self.dialog:
-            self.dialog = MDDialog(
-                title="Add Appointment:",
-                type="custom",
-                content_cls=Content(),
-                buttons=[
-                    MDFlatButton(
-                        text="CANCEL",
-                        theme_text_color="Custom",
-                        text_color=self.theme_cls.primary_color,
-                        on_release=self.on_cancel
-                    ),
-                    MDFlatButton(
-                        text="OK",
-                        theme_text_color="Custom",
-                        text_color=self.theme_cls.primary_color,
-                        on_release=lambda *args: self.save_appointment()
-                    ),
-                ],
-            )
-        self.dialog.open()
+    # def show_add_appointment_popup(self):
+    #     self.dialog = MDDialog(
+    #     title="Add Appointment:",
+    #     type="custom",
+    #     content_cls=Content(),
+
+    #     )
+    #     self.dialog.open()
 
     def show_error_popup(self, message):
         content = Label(text=message)
-        error_popup = Popup(title='Error', content=content, size_hint=(None, None), size=(300, 200))
+        error_popup = Popup(title='Error', content=content,
+                            size_hint=(None, None), size=(300, 200))
         error_popup.open()
 
-    def on_cancel(self, *args):
+    def on_cancel_date(self, *args):
         self.date_picker_open = False
-        self.dialog.dismiss()
 
-    def on_save(self, instance, value, date_range):
-        try:
-            self.dialog.content_cls.ids.date_input.text = str(value)  
-        except ValueError:
-            self.show_error_popup('Invalid Date Format')
-        finally:
-            self.date_picker_open = False
+    def on_save_date(self, instance, value, date_range):
+        self.ids.date_input.text = str
+        self.date_picker_open = False
+
+    def on_cancel_time(self, *args):
+        self.time_picker_open = False
+
+    def on_save_time(self, instance, time):
+        self.ids.time_input.text = time.strftime("%H:%M")
+        self.time_picker_open = False
 
     def show_date_picker(self):
         if not hasattr(self, 'date_picker_open') or not self.date_picker_open:
             self.date_picker_open = True
-            date_dialog = MDDatePicker()
-            date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+            screen_width, screen_height = Window.size
+            picker_width = screen_width * 0.8  # Adjust as needed
+            picker_height = screen_height * 0.6  # Adjust as needed
+            date_dialog = MDDatePicker(min_date=date.today(), max_date=date(
+                date.today().year,
+                date.today().month+6,
+                date.today().day,
+            ),
+            )
+            date_dialog.size_hint_max = (picker_width, picker_height)
+            date_dialog.size_hint_min = (picker_width, picker_height)
+            # date_dialog = MDDatePicker()
+            date_dialog.bind(on_save=self.on_save_date,
+                             on_cancel=self.on_cancel_date)
             date_dialog.open()
+        
 
+    def show_time_picker(self):
+        if not hasattr(self, 'time_picker_open') or not self.time_picker_open:
+            self.time_picker_open = True
+            time_dialog = MDTimePicker()
+            time_dialog.bind(on_save=self.on_save_time,
+                             on_cancel=self.on_cancel_time)
+            time_dialog.open()
+        
 
 class HospitalWindow(Screen):
     def on_enter(self):
