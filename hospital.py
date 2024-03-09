2b5fecba96b24ff6166cf9c4ed7681d9a104f1fa
sandipkatel <babusandipkatel@gmail.com>
2024-03-09 22:47:59 +0545

    removed something.py

--- hospital.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import csv
-def readData():
-    df = pd.read_csv("dataset\hospitals.csv", encoding="ISO-8859-1")
-    data_list = df.T.values.tolist()
-    return data_list
-
-
