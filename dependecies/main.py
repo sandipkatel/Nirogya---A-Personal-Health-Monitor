0dd7dc9a4186862e0e18c5ac34b7e65d03259d58
saphalr <saphalrimal@gmail.com>
2024-03-09 20:36:16 +0545

    scheduler and katel chor

--- dependecies/main.py
+++ dependecies/main.py
@@ -1,7 +1,6 @@
 from kivymd.app import MDApp
 import visual as vs
-# import predictor as pr
-from predictor import Prediction
+import predictor as pr
 from kivy.core.window import Window
 
 Window.size = (360, 640)
@@ -9,58 +8,45 @@ Window.size = (360, 640)
 
 class MyMainApp(MDApp):
     symptoms_list = []
-    pr = Prediction()
     predictor_window = None
     scheduler_window = None
-    doctor_window = None
     hospital_window = None
     symptoms_window = None
+    doctor_window = None
+    reminder_window = None
 
     def build(self):
         wm = vs.WindowManager()
         self.predictor_window = vs.PredictorWindow(name='predictor')
+        self.doctor_window = vs.DoctorWindow(name='doctor')
         self.scheduler_window = vs.SchedulerWindow(name='scheduler')
         self.hospital_window = vs.HospitalWindow(name='hospital')
         self.symptoms_window = vs.SymptomsWindow(name='symptoms')
-        self.doctor_window = vs.DoctorWindow(name='doctor')
-        self.data_window = vs.DataWindow(name='data')
-        self.blood_window = vs.BloodWindow(name='blood')
-        self.graph_window = vs.GraphWindow(name='graph')
-        self.month_window = vs.MonthWindow(name='month')
+        self.reminder_window = vs.ReminderWindow(name='reminder')
+        
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Blue"
         wm.add_widget(vs.HomeWindow(name='home'))
-        wm.add_widget(self.predictor_window)
         wm.add_widget(self.doctor_window)
+        wm.add_widget(self.predictor_window)
         wm.add_widget(self.scheduler_window)
         wm.add_widget(self.hospital_window)
         wm.add_widget(self.symptoms_window)
-        wm.add_widget(self.data_window)
-        wm.add_widget(self.blood_window)
-        wm.add_widget(self.graph_window)
-        wm.add_widget(self.month_window)
+        wm.add_widget(self.reminder_window)
         return wm
 
     def on_start(self):
-        self.symptoms_list = list(self.pr.get_columns())
+        self.symptoms_list = list(pr.df1.columns)
         if self.predictor_window:
             self.predictor_window.on_enter()
+            pass
         if self.scheduler_window:
             self.scheduler_window.on_enter()
-        if self.doctor_window:
-            self.doctor_window.on_enter()
+            pass
         if self.hospital_window:
             self.hospital_window.on_enter()
         if self.symptoms_window:
             self.symptoms_window.on_enter()
-        if self.data_window:
-            self.data_window.on_enter()
-        if self.blood_window:
-            self.blood_window.on_enter()
-        if self.graph_window:
-            self.graph_window.on_enter()
-        if self.month_window:
-            self.month_window.on_enter()
 
 
 if __name__ == "__main__":
--- scheduler.py
+++ scheduler.py
@@ -1,38 +1,110 @@
-class PriorityQueue:
-    def __init__(self):
-        self.queue = []
+import json
+from datetime import datetime
 
-    def push(self, item, priority):
-        self.queue.append((priority, item))
-        self.queue.sort(key=lambda x: x[0])
+# Linked List Node
+class PriorityQueueNode: 
+      
+    def __init__(self, value, pr): 
+        self.data = value 
+        self.priority = pr 
+        self.next = None
+          
+# Implementation of Priority Queue 
+class PriorityQueue: 
+      
+    def __init__(self): 
+        self.front = None
+          
+    # Method to check if the Priority Queue is Empty  
+    def isEmpty(self): 
+        return True if self.front == None else False
+      
+    # Method to add items in Priority Queue  
+    # According to their priority value 
+    def push(self, value, priority): 
+        if self.isEmpty(): 
+            self.front = PriorityQueueNode(value, priority) 
+            return 1 
+        else: 
+            if self.front.priority > priority: 
+                newNode = PriorityQueueNode(value, priority) 
+                newNode.next = self.front 
+                self.front = newNode 
+                return 1
+            else: 
+                temp = self.front 
+                while temp.next: 
+                    if priority <= temp.next.priority: 
+                        break
+                    temp = temp.next
+                newNode = PriorityQueueNode(value, priority) 
+                newNode.next = temp.next
+                temp.next = newNode 
+                return 1 
+      
+    # Method to remove a node with a specific value from the Priority Queue 
+    def remove(self, time, name): 
+        if self.isEmpty(): 
+            return "Queue is Empty!"
+        else:   
+            temp = self.front
+            # If the node to be removed is the first node
+            if temp.data ==(time, name):
+                self.front = temp.next
+                temp = None
+                return 1
+                
+            while temp.next:
+                if temp.next.data == (time, name):
+                    temp.next = temp.next.next
+                    return 1
+                temp = temp.next
+            return "Value not found in the queue"
+      
+    # Method to remove high priority item 
+    def pop(self): 
+        if self.isEmpty(): 
+            return "Queue is Empty!"
+        else:   
+            self.front = self.front.next
+            return 1
+              
+    # Method to return high priority node  
+    def peek(self): 
+        if self.isEmpty(): 
+            return "Queue is Empty!"
+        else: 
+            return self.front.data
+    def clear(self):
+        self.front = None 
+              
+    # Method to Traverse through Priority Queue 
+    def traverse(self): 
+        if self.isEmpty(): 
+            return "Queue is Empty!"
+        else: 
+            temp = self.front 
+            while temp: 
+                print(temp.data, end=" ") 
+                temp = temp.next
+            print()
 
-    def pop(self):
-        if self.queue:
-            return self.queue.pop(0)[1]
-        else:
-            return None
-
-    def peek(self):
-        if self.queue:
-            return self.queue[0][1]
-        else:
-            return None
-
-    def remove(self, item):
-        self.queue = [(priority, value)
-                      for priority, value in self.queue if value != item]
 
 
 class AppointmentScheduler:
     def __init__(self):
         self.appointments = PriorityQueue()
+        # self.filename = filename
+        # self.load_from_file()
 
     def schedule_appointment(self, appointment_time, patient_name):
         self.appointments.push(
             (appointment_time, patient_name), appointment_time)
 
     def cancel_appointment(self, appointment_time, patient_name):
-        self.appointments.remove((appointment_time, patient_name))
+        self.appointments.remove(appointment_time, patient_name)
 
     def next_appointment(self):
         return self.appointments.peek()
+    
+
